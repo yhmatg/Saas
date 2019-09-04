77352e09dd84f0c9de8cdd117e80d6851ce05653
haomiao.yin <haomiao.yin@esimtek.com>
2019-09-04 08:41:31 +0000

    Revert "Merge branch 'cherry-pick-c1a8e233' into 'nanhua_develop'"
    This reverts merge request !2

--- app/src/main/java/com/common/esimrfid/ui/invdetail/InvdetailActivity.java
+++ app/src/main/java/com/common/esimrfid/ui/invdetail/InvdetailActivity.java
@@ -465,14 +465,12 @@ public class InvdetailActivity extends BaseActivity<InvDetailPresenter> implemen
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(!STATUS_FINISH.equals(mStatus)){
-            if (esimUhfService != null) {
-                if (keyCode == esimUhfService.getDownKey()) { //扳机建扫描
-                    esimUhfService.startStopScanning();
-                }
-            } else if(keyCode == Utils.getDiffDownKey()) {
-                ToastUtils.showShort(R.string.not_connect_prompt);
+        if (esimUhfService != null) {
+            if (keyCode == esimUhfService.getDownKey()) { //扳机建扫描
+                esimUhfService.startStopScanning();
             }
+        } else if(keyCode == Utils.getDiffDownKey()) {
+            ToastUtils.showShort(R.string.not_connect_prompt);
         }
         return super.onKeyDown(keyCode, event);
     }
