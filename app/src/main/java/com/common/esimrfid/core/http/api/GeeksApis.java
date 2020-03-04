5bf9b7f3de301848025006144f72ed70f9495b72
haomiao.yin <haomiao.yin@esimtek.com>
2020-03-04 14:29:22 +0800

    add some note and delete some uesless file"

--- app/src/main/java/com/common/esimrfid/core/http/api/GeeksApis.java
+++ app/src/main/java/com/common/esimrfid/core/http/api/GeeksApis.java
@@ -39,107 +39,183 @@ import retrofit2.http.Query;
 
 public interface GeeksApis {
 
-    String HOST = "http://172.16.63.25:12000"; //佳航25，言娇35，梦伟32
-
     //登录
+    //@param UserInfo 用户账号 用户密码
+    //@return 用户基本信息,token
+
     @POST("user-server/userauth/loginwithinfo")
     Observable<BaseResponse<UserLoginResponse>> login(@Body UserInfo userInfo);
 
     //获取盘点数据
-    //@GET("inventory-server/inventoryorders")
+    //@param userId 用户userId
+    //@return 用户分配的盘点任务列表
+
     @GET("inventory-server/inventoryorders/unpage")
     Observable<BaseResponse<List<ResultInventoryOrder>>> fetchAllIvnOrders(@Query("user_id")String userId);
 
     //获取盘点条目详情
+    //@param orderId 盘点单id
+    //@return 盘点单详情
+
     @GET("inventory-server/inventoryorders/{orderId}/detail")
     Observable<BaseResponse<ResultInventoryDetail>>  fetchAllInvDetails(@Path("orderId") String orderId);
 
     //完成盘点
+    //@param orderId 盘点单id
+    //@param uid 用户id
+    //@param remark 盘点单备注
+    //@return 操作结果
+
     @POST("inventory-server/inventoryorders/{orderId}/finish")
     Observable<BaseResponse> finishInvOrder(@Path("orderId")String orderId,@Query("uid")String uid,@Query("finish_remark")String remark);
 
     //完成盘点携带资产编号id
+    //@param orderId 盘点单id
+    //@param uid 用户id
+    //@param invDetails 已盘点资产id集合
+    //@return 操作结果
+
     @POST("inventory-server/inventoryorders/{orderId}/finishwithinfo")
     Observable<BaseResponse> finishInvOrderWithAsset(@Path("orderId")String orderId,@Query("uid")String uid,@Body List<String> invDetails);
 
     //盘点数据上传
+    //@param orderId 盘点单id
+    //@param invDetails 已盘点资产id集合
+    //@param uid 用户id
+    //@return 操作结果
+
     @POST("inventory-server/inventoryorders/{orderId}/commit")
     Observable<BaseResponse> uploadInvDetails(@Path("orderId")String orderId,@Body List<String> invDetails ,@Query("uid")String uid);
 
     //获取查询数据
+    //@param ecps 资产EPC集合
+    //@return 资产详情集合
+
     @POST("assets-server/assets/byrfids")
     Observable<BaseResponse<List<AssetsInfo>>> fetchScanAssetsInfons(@Body Set<String> ecps);
 
     //获取领用单列表
+    //@return 领用单列表
+
     @GET("/assets-server/requisitions/unpage")
     Observable<BaseResponse<List<RequisitionItemInfo>>> fetchAllRequisitions();
 
     //通过单号获取领用单详情
+    //@param odrId 领用单id
+    //@return 领用单详情
+
     @GET("/assets-server/requisitions/{odrId}")
     Observable<BaseResponse<RequisitionDetailInfo>> fetchRequsitionDetailByid(@Path("odrId")String odrId);
 
     //模糊查询资产详情
+    //@param patternName 资产过滤信息
+    //@return 查询资产列表
+
     @GET("/assets-server/assets/unpage")
     Observable<BaseResponse<List<RequisitionAssetInfo>>> fetchRequestAssetsInfos(@Query("pattern_name")String patternName);
 
     //确认领用资产后上传 id 领用单id
+    //@param requestId 领用单id
+    //@param epcs 领用的资产id
+    //@return 操作结果
+
     @POST("/assets-server/requisitions/{id}/reqAssets")
     Observable<BaseResponse> uploadResAssets(@Path("id")String requestId,@Body List<String> epcs);
 
     //根据epc获取资产详情
+    //@param ecps 资产EPC
+    //@return 资产列表
+
     @POST("assets-server/assets/byrfids")
     Observable<BaseResponse<List<RequisitionAssetInfo>>> getRequisitionInfons(@Body Set<String> ecps);
 
     //获取不同位置下资产数量
+    //@return 位置和对应资产数目
+
     @GET("/assets-server/assets/countbyloc")
     Observable<BaseResponse<HashMap<String,Integer>>> getAssetsNmbDiffLocation();
 
     //获取不同状态的资产数量
+    //@return 不同状态下的资产数目
+
     @GET("/assets-server/assets/countbystatus")
     Observable<BaseResponse<AssetStatusNum>> getAssetsNmbDiffStatus();
 
     //查询所有管理员
+    //@return 所有管理员信息
+
     @GET("/user-server/sysusers/querymanager/unpage")
-    //@GET("/user-server/emps/all/unpage")
     Observable<BaseResponse<List<MangerUser>>> getAllManagerUsers();
 
     //查询所有公司
+    //@return 所有公司信息
+
     @GET("/user-server/orgs/corps")
     Observable<BaseResponse<List<CompanyBean>>> getAllCompany();
 
     //获取公司下的所有部门
+    //@param comId 公司id
+    //@return 公司下所有部门
+
     @GET("/user-server/orgs/{comId}/subs")
     Observable<BaseResponse<List<DepartmentBean>>> getAllDeparts(@Path("comId")String comId);
 
     //获取所有资产类型
+    //@return
+    //所有资产类型
     @GET("/assets-server/assetstypes")
     Observable<BaseResponse<List<AssetsType>>> getAllAssetsType();
 
     //获取所有资产位置
+    //@return 所有资产位置
+
     @GET("/assets-server/locations")
     Observable<BaseResponse<List<AssetsLocation>>> getAllAssetsLocation();
 
     //创建盘点单
+    //@param inv_name 盘点单名称（必要）
+    //@param inv_assigner_id 盘点人（必要）
+    //@param inv_exptfinish_date 预期完成时间（必要）
+    //@param inv_used_corp_filter 使用公司
+    //@param inv_belong_corp_filter 所属公司
+    //@param inv_used_dept_filter 使用部门
+    //@param inv_type_filter 资产分类
+    //@param inv_loc_filter 存放地点
+    //@return 新建盘点单详情
+
     @POST("/inventory-server/inventoryorders")
     Observable<BaseResponse<CreateInvResult>> createNewInventory(@Body InventoryParameter invpara);
 	
 	//模糊查询资产详情（写入标签）
+    //@param patternName 资产过滤信息
+    //@return 资产列表
+
     @GET("/assets-server/assets/unpage")
     Observable<BaseResponse<List<AssetsInfo>>>fetchWriteAssetsInfos(@Query("pattern_name")String patternName);
 
     //根据Epc查询资产详情（资产查找）
+    //@param Epcs EPC集合
+    //@return 资产信息列表
+
     @POST("/assets-server/assets/byrfids")
     Observable<BaseResponse<List<AssetsInfo>>> fetchScanAssets(@Body Set<String> Epcs);
+
+    //根据资产id获取资产详情
+    //@param astid 资产id
+    //@return 资产详情信息
 
     @GET("/assets-server/assets/{astid}")
     Observable<BaseResponse<AssetsDetailsInfo>> fetchAssetsInfoById(@Path("astid") String astid);
 
     //版本更新
+    //@return 版本更新详情
+
     @GET("/tagorder-server/appversions/latest")
     Observable<BaseResponse<UpdateVersion>> updateVersion();
 
     //获取公司信息详情
-    //@GET("/user-server/orgs/selectCurrentOrg")
+    //@return 公司详情信息
+
     @GET("/user-server/sysusers/selectCurrentOrg")
     Observable<BaseResponse<CompanyInfo>> getCompanyInfo();
 }
