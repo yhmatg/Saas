873b1d564483e609037e9dd8a2b835da40ddae28
zhimin <zhimin.wang@esimtek.com>
2020-04-26 15:33:01 +0800

    add assets use status

--- app/src/main/java/com/ddcommon/esimrfid/core/bean/emun/AssetsUseStatus.java
+++ app/src/main/java/com/ddcommon/esimrfid/core/bean/emun/AssetsUseStatus.java
@@ -9,8 +9,18 @@ public enum AssetsUseStatus {
     IN_IDEL("闲置",0),
     IN_USED("在用", 1),
     IN_REPAIR("维修中", 2),
-    IN_Transfer("调拨中",3);
-
+    IN_Transfer("调拨中",3),
+    IN_DISTRIBUTE("待派发",4),
+    AL_DISTRIBUTE("已派发",5),
+    IN_BORROW("借用",6),
+    APPROVAL_BORROW("借用审批中",7),
+    APPROVAL_RETURN("归还审批中",8),
+    APPROVAL_REPAIR("维修审批中",9),
+    SCRAP("报废",10),
+    APPROVAL_ALLOT("调拨审批中",11),
+    APPROVAL_SCRAP("报废审批中",12),
+    APPROVAL_REQUISITION("领用审批中",13),
+    APPROVAL_RE_STORE("退库审批中",14);
 
     // 成员变量
     private String name;
