aa965d4b4e7c1a3662d8e515a59daf5f9397e7ba
haomiao.yin <haomiao.yin@esimtek.com>
2020-09-02 14:52:08 +0800

    modify page for AssetInventoryActivity

--- app/src/main/java/com/common/esimrfid/ui/assetinventory/AssetInventoryActivity.java
+++ app/src/main/java/com/common/esimrfid/ui/assetinventory/AssetInventoryActivity.java
@@ -10,17 +10,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.common.esimrfid.R;
-import com.common.esimrfid.app.EsimAndroidApp;
 import com.common.esimrfid.base.activity.BaseActivity;
 import com.common.esimrfid.contract.home.InvOrderContract;
 import com.common.esimrfid.core.DataManager;
 import com.common.esimrfid.core.bean.nanhua.BaseResponse;
-import com.common.esimrfid.core.bean.nanhua.jsonbeans.InventoryDetail;
 import com.common.esimrfid.core.bean.nanhua.jsonbeans.ResultInventoryDetail;
 import com.common.esimrfid.core.bean.nanhua.jsonbeans.ResultInventoryOrder;
 import com.common.esimrfid.presenter.home.InvOrderPressnter;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import java.util.ArrayList;
@@ -57,13 +56,17 @@ public class AssetInventoryActivity extends BaseActivity<InvOrderPressnter> impl
     @BindView(R.id.divide_line)
     View lineView;
     InvTaskAdapter mAdapter;
-    List<ResultInventoryOrder> mInvTaskorders = new ArrayList<>();
+    //List<ResultInventoryOrder> mInvTaskorders = new ArrayList<>();
     List<ResultInventoryOrder> mUnFinishedTaskorders = new ArrayList<>();
     List<ResultInventoryOrder> mFinishedTaskorders = new ArrayList<>();
     List<ResultInventoryOrder> mShowTaskorders = new ArrayList<>();
-    private Boolean isFirstOnResume = true;
+    //private Boolean isFirstOnResume = true;
     private String userId;
     private Boolean isShowUnfinish = true;
+    private int currentPage = 1;
+    private int pageSize = 10;
+    private boolean isNeedClearData;
+    private int invordersSize = 0;
 
     @Override
     public InvOrderPressnter initPresenter() {
@@ -80,25 +83,39 @@ public class AssetInventoryActivity extends BaseActivity<InvOrderPressnter> impl
         mRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
             public void onRefresh(@NonNull RefreshLayout refreshLayout) {
-                mPresenter.fetchAllIvnOrders(userId, true);
+                isNeedClearData = true;
+                currentPage = 1;
+                //mPresenter.fetchAllIvnOrders(userId, true);
+                mPresenter.fetchAllIvnOrdersPage(pageSize,1,0,userId,true);
+            }
+        });
+        mRefreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                isNeedClearData = false;
+                currentPage ++;
+                mPresenter.fetchAllIvnOrdersPage(pageSize,currentPage,invordersSize,userId,true);
             }
         });
         mRefreshLayout.setEnableAutoLoadMore(false);//使上拉加载具有弹性效果
         mRefreshLayout.setEnableOverScrollDrag(false);//禁止越界拖动（1.0.4以上版本）
         mRefreshLayout.setEnableOverScrollBounce(false);//关闭越界回弹功能
+        mPresenter.fetchAllIvnOrdersPage(pageSize,1,0,userId,true);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //初始化或者更新本地盘点的状态
-        if (isFirstOnResume) {
+        /*if (isFirstOnResume) {
             mPresenter.fetchAllIvnOrders(userId, true);
             isFirstOnResume = false;
         } else {
             mPresenter.fetchAllIvnOrders(userId, true);
-        }
-
+        }*/
+       /* isNeedClearData = true;
+        currentPage = 1;
+        mPresenter.fetchAllIvnOrdersPage(pageSize,1,0,userId,true);*/
     }
 
     @Override
@@ -169,13 +186,18 @@ public class AssetInventoryActivity extends BaseActivity<InvOrderPressnter> impl
                 return o2.getCreate_date().compareTo(o1.getCreate_date());
             }
         });
-        mInvTaskorders.clear();
-        mFinishedTaskorders.clear();
-        mUnFinishedTaskorders.clear();
+        if(isNeedClearData){
+            mRefreshLayout.finishRefresh();
+            mFinishedTaskorders.clear();
+            mUnFinishedTaskorders.clear();
+            invordersSize = 0;
+        }else {
+            mRefreshLayout.finishLoadMore();
+        }
         mShowTaskorders.clear();
-        mInvTaskorders.addAll(resultInventoryOrders);
-        for (int i = 0; i < mInvTaskorders.size(); i++) {
-            ResultInventoryOrder resultInventoryOrder = mInvTaskorders.get(i);
+        invordersSize += resultInventoryOrders.size();
+        for (int i = 0; i < resultInventoryOrders.size(); i++) {
+            ResultInventoryOrder resultInventoryOrder = resultInventoryOrders.get(i);
             if (resultInventoryOrder.getInv_status() == 10) {
                 mUnFinishedTaskorders.add(resultInventoryOrder);
             } else if (resultInventoryOrder.getInv_status() == 11) {
